ba0ad1fa76d96cfcbbebddd710edf51d584c2bd6
samidh93 <sami.dhiab.x@gmail.com>
2025-08-26 00:48:57 +0200

    ..

--- src/frontend/src/components/Todo/TodoList.tsx
+++ src/frontend/src/components/Todo/TodoList.tsx
@@ -59,7 +59,7 @@ const TodoList: React.FC = () => {
     } finally {
       setLoading(false);
     }
-  }, [page, rowsPerPage, filters]);
+  }, [page, rowsPerPage]);
 
   useEffect(() => {
     fetchTodos();
