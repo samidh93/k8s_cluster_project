cd56620b76d37e4041d7574da4c7d3a0cbc69d2b
samidh93 <sami.dhiab.x@gmail.com>
2025-09-08 13:38:17 +0100

    Fix frontend API URL to use relative path for nginx proxy

--- src/frontend/src/services/todoApi.ts
+++ src/frontend/src/services/todoApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Todo, TodoFormData, TodoStatistics, PaginatedTodos } from '../types/Todo';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '/api';
 
 // Create axios instance with default config
 const apiClient = axios.create({
